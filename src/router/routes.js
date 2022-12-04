436e3a05123596b44d33bdbe2104859ca697b2fe
mylittleZ <1020464190@qq.com>
2022-12-04 17:55:02 +0800

    feat: set default redirect route

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,6 +1,10 @@
 
 export default [
   {
+    path: '/',
+    redirect: '/video-face-traking'
+  },
+  {
     path: "/video-face-traking",
     name: "Video人脸识别",
     component: () => import("@/views/VideoFaceRecognition.vue"),
